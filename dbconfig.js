4d2f12903ea937727d6b6c3d706aabbf9d8c2a7e
HADAPARI <davs.richard1@gmail.com>
2021-04-01 16:58:08 +0300

    maj

--- dbconfig.js
+++ dbconfig.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host:process.env.DATABASE_HOST,
-    port:process.env.DATABASE_PORT,
+    database:process.env.DATABASE_NAME,
     user:process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE_NAME
+    password:process.env.DATABASE_PASSWORD
 });
 
 db.connect();
